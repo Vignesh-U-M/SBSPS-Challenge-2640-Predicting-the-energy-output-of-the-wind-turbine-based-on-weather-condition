858a9b84d76261c2aea515c27d5fb68665ac477f
Muthukumar T <tmuthukumarnpm@gmail.com>
2020-06-24 17:36:40 +0530

    Map Functionality added

--- Windenergy_prediction/production/urls.py
+++ Windenergy_prediction/production/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('main', views.main, name="main"),
-
+    path('map', views.map, name="map"),
+    path('add_location_map', views.add_location_map, name="add_location_map")
 ]
--- Windenergy_prediction/production/views.py
+++ Windenergy_prediction/production/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.models import auth, User
+import folium
 # Create your views here.
 def main(request):
     username = User.objects.get(username=request.user.username)
+    return render(request, "user_home.html", {"username": username})
+
+def map(request):
+    return render(request,"map.html")
+
+def add_location_map(request):
+    username = User.objects.get(username=request.user.username)
+    lat = request.POST['lot']
+    lon = request.POST['log']
+    map = folium.Map(location=[lat,lon], zoom_start=6,tiles="Stamen Terrain")
+    fg = folium.FeatureGroup(name="test")
+    fg.add_child(folium.Marker(location=[lat, lon], popup="Location Name", icon=folium.Icon(color='green')))
+    map.add_child(fg)
+    map.save("Templates/map.html")
     return render(request, "user_home.html", {"username": username})
